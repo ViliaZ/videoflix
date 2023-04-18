fbafaa7c37b26571bc1f309d48b61b4155623e33
ViliaZ <vzeisig@gmail.com>
2023-04-18 07:31:46 +0200

    remove sidebar from main template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent,
-    SidebarComponent
+    MainComponent
   ],
   imports: [
     BrowserModule,
